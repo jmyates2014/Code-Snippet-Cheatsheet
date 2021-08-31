e3c54de9c10e25f1fc611ac40ab875b5ba4b73c9
jmyates2014 <jmyates@gmail.com>
2021-08-31 12:32:17 -0400

    Added Strapi Server

---
+++ Server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '79aeae85b3a2d84745474687bfd1c058'),
+    },
+  },
+});
---
+++ Server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '88705bf1-803b-4d34-b28d-c36c292fc48e'
+};
