b299d0ef31ea21b6536a2043c0a763178cc68ddc
jmyates2014 <jmyates@gmail.com>
2021-09-02 11:29:37 -0400

    Updated Footer

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -5,11 +5,22 @@ const Footer = () => {
   return (
     <div className="bg-dark text-light fixed-bottom text-center py-3">
       <div className="container">
-        <span>Footer Component will be here</span>
-        <br></br>
-        <a href="http://localhost:1337/admin" target="_blank">
-          Admin Area
-        </a>
+        <div className="row">
+          <div className="col-4">
+            <a href="http://localhost:1337/admin" target="_blank">
+              Admin Area
+            </a>
+          </div>
+          <div className="col-4">Footer Component will be here</div>
+          <div className="col-4">
+            <a
+              href="https://github.com/jmyates2014/Code-Snippet-Cheatsheet"
+              target="_blank"
+            >
+              GitHub Repo
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
